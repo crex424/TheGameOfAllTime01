dbad16db2fea1287cc04946ec07d7bc5c4aa541d
Campbell Rex <36382100+crex424@users.noreply.github.com>
2026-09-22 21:48:59 +0000

    Created boundries. Teleport bug not fixed.

--- game/src/Main.cpp
+++ game/src/Main.cpp
@@ -2,13 +2,11 @@
 #include <bn_core.h>
 #include <bn_keypad.h>
 #include <bn_sprite_ptr.h>
-
 #include "bn_sprite_items_dot.h"
 
 int main()
 {
     bn::core::init();
-
     bn::backdrop::set_color(bn::color(31, 0, 0));
 
     auto dot = bn::sprite_items::dot.create_sprite(0, 0);
@@ -19,19 +17,47 @@ int main()
     {
         if (bn::keypad::left_held())
         {
-            dot.set_x(dot.x() - speed);
+            if (dot.x() >= 130)
+            {
+                dot.set_x(-130);
+            }
+            else
+            {
+                dot.set_x(dot.x() - speed);
+            }
         }
         if (bn::keypad::right_held())
         {
-            dot.set_x(dot.x() + speed);
+            if (dot.x() <= -130)
+            {
+                dot.set_x(130);
+            }
+            else
+            {
+                dot.set_x(dot.x() + speed);
+            }
         }
         if (bn::keypad::up_held())
         {
-            dot.set_y(dot.y() - speed);
+            if (dot.y() <= -90)
+            {
+                dot.set_y(90);
+            }
+            else
+            {
+                dot.set_y(dot.y() - speed);
+            }
         }
         if (bn::keypad::down_held())
         {
-            dot.set_y(dot.y() + speed);
+            if (dot.y() >= 90)
+            {
+                dot.set_y(-90);
+            }
+            else
+            {
+                dot.set_y(dot.y() + speed);
+            }
         }
         bn::core::update();
     }
